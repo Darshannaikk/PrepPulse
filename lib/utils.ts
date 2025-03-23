740b5afb126e559e08bb3a58dba5b13cfa1ca08a
Darshan Naik <naikdarshan1608@gmail.com>
2025-03-23 18:06:08 +0530

    Made some changes

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,9 +1,3 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
 import { interviewCovers, mappings } from "@/constants";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
